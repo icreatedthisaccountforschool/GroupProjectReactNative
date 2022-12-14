5e44fbc368f69129d746e4eacb9ae564d24bcf2e
GHC <93799226+icreatedthisaccountforschool@users.noreply.github.com>
2022-12-14 18:39:21 -0500

    Delete index.js

--- actionTypes/index.js
+++
@@ -1,6 +0,0 @@
-export const = "";
-export const = "";
-
-//the action types have to be named and written the same btw export const and the quotations
-//whatever action types you want is up to you and the other group members
-//default action types as defined in the week 6 slides are COUNTER_INCREASE and COUNTER_DECREASE
