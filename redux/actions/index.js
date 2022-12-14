d733840a9036bf6a4b60e07cb987915b4cfe0d45
GHC <93799226+icreatedthisaccountforschool@users.noreply.github.com>
2022-12-14 18:35:54 -0500

    Create index.js

---
+++ redux/actions/index.js
@@ -0,0 +1,20 @@
+import {} from "../actionTypes";
+
+export const = => ({
+  type: ,
+  payload: {
+    
+  }
+});
+
+export const = => ({
+  type: ,
+  payload: {
+    
+  }
+});
+
+//type is the variables defined in actionTypes, you can decide what to place inside
+//make up whatever variables you'd like btw const and =, and btw = and =>
+//refer to the week 6 slides and any online resource for Redux to finish this
+//you may copy and paste these templates as many times as necessary for the app
